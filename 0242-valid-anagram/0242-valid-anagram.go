b1f3ea79db893fa6cc389faf75f6e8c0fecd49dd
Adryan Eka Vandra <adryanekavandra@gmail.com>
2024-09-07 23:21:15 +0700

    Time: 3 ms (77.86%), Space: 3.1 MB (61.47%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.go
@@ -0,0 +1,23 @@
+import "strings"
+
+func isAnagram(s string, t string) bool {
+	m := make(map[rune]int)
+	
+
+	for _, v := range s {
+		m[v]++
+	}
+
+	for _, v := range t {
+		m[v]--
+	}
+
+	for _, v := range m {
+        if v != 0 {
+            return false
+        }
+    }
+
+    return true
+
+}
