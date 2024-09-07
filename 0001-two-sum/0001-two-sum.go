ddf2db4fdc544aa376cc5d8650a4ddad346b1a2f
Adryan Eka Vandra <adryanekavandra@gmail.com>
2024-09-07 19:13:25 +0700

    Time: 5 ms (75.9%), Space: 4.1 MB (58.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,21 +1,12 @@
 func twoSum(nums []int, target int) []int {
-    var res []int
+    m := make(map[int]int)
 
-    for i := 0; i < len(nums); i++ {
-        
-        if len(res) == 2 {
-            break;
+    for i, num := range nums {
+        complement := target - num
+        if j, ok := m[complement]; ok && j != i {
+            return []int{i, j}
         }
-        for j := i+1; j < len(nums) ; j++ {
-            sum := nums[i] + nums[j]
-
-            if (sum) == target {
-                res = append(res, i)
-                res = append(res, j)
-                break;
-            }
-        }
+        m[num] = i
     }
-
-    return res
+    return nil
 }
