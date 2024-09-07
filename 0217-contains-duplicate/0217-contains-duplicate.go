7f997b90ea84ddb0bbc99a604afb3e61878f4541
Adryan Eka Vandra <adryanekavandra@gmail.com>
2024-09-07 19:21:59 +0700

    Time: 62 ms (97.97%), Space: 11.4 MB (31.8%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -0,0 +1,11 @@
+func containsDuplicate(nums []int) bool {
+    m := make(map[int]int)
+
+    for i, v := range nums {
+        if _,ok := m[v]; ok {
+            return true;
+        }
+        m[v] = i
+    }
+    return false
+}
