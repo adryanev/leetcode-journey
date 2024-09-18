6714b43227f5ac2e1b26af96cd75e65ed04d8d7a
Adryan Eka Vandra <adryanekavandra@gmail.com>
2024-09-18 21:31:42 +0700

    Time: 2 ms (31.69%), Space: 2.9 MB (25.33%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.go
@@ -0,0 +1,24 @@
+func mergeAlternately(word1 string, word2 string) string {
+    res := ""
+    i,j := 0, 0
+
+    for i < len(word1) && j < len(word2) {
+        res += string(word1[i]) + string(word2[j])
+        i++;
+        j++;
+    }
+
+    for i < len(word1) {
+        res += string(word1[i])
+        i++
+    }
+
+    for i < len(word2) {
+        res += string(word2[i])
+        i++
+    }
+
+
+    return res
+
+}
